7b66ecb04abb11ba89917df7eb4912889476c095
Hiroki Nohara <hnohara@c1r1s6.42tokyo.jp>
2021-01-05 22:18:28 +0900

    feat(ft_strjoin): Catch null is given

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,11 +6,11 @@
 /*   By: hnohara <hnohara@student.42tokyo.j>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 13:45:00 by hnohara           #+#    #+#             */
-/*   Updated: 2020/11/11 13:53:38 by hnohara          ###   ########.fr       */
+/*   Updated: 2021/01/05 22:16:56 by hnohara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -18,12 +18,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		n2;
 	char	*p;
 
-	n1 = 0;
-	while (s1[n1])
-		++n1;
-	n2 = 0;
-	while (s2[n2++])
-		++n1;
+	if (!s1 || !s2)
+		return (NULL);
+	n1 = ft_strlen(s1);
+	n2 = ft_strlen(s2);
+	n1 += n2;
 	p = (char*)malloc(sizeof(char) * (n1 + 1));
 	if (!p)
 		return (NULL);
